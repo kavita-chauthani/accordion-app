3cc9339c70ab5975145ec7af3719f54d0e3811db
kavita chauthani <kckabu81@gmail.com>
2024-07-01 11:24:38 +0530

    completed accordion app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "These chairs are assembled in different corner",
+  },
+  {
+    title: "Where are these chairs assembled?",
+    text: "These chairs are assembled in different corner",
+  },
+  {
+    title: "Where are these chairs assembled?",
+    text: "These chairs are assembled in different corner",
+  },
+  {
+    title: "Where are these chairs assembled?",
+    text: "These chairs are assembled in different corner",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <>
+      <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+        <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+        <p className="title">{title}</p>
+        <p className="icon">{isOpen ? "-" : "+"}</p>
+        {isOpen && <div className="content-box">{text}</div>}
+      </div>
+    </>
   );
 }
 
